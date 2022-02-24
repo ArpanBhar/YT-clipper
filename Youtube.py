eea56366783a767641d558edd6e3b8a5ff997f3c
DeathSlayer <96098236+ArpanBhar@users.noreply.github.com>
2022-02-24 20:24:37 +0530

    Now enter the ending time (Was time to forward the video).

--- Youtube.py
+++ Youtube.py
@@ -15,10 +15,19 @@ def rs(n):
         diff = "00:00"
         passed = False
     return diff
+def diff(n,z):
+    n = str(n).split(":")
+    z = str(z).split(":")
+    t = datetime.strptime(":".join(n),"%M:%S") if len(n) == 2 else datetime.strptime(":".join(n),"%H:%M:%S")
+    t2 = datetime.strptime(":".join(z),"%M:%S") if len(z) == 2 else datetime.strptime(":".join(z),"%H:%M:%S")
+    dif = str(t-t2).split(":")
+    if int(dif[0]) == 0:
+        del dif[0]
+    return ":".join(dif)
 link = input("Enter the video link: ")
 timestamp = input("Enter the starting time: ")
-seek = input("Enter the time to seek: ")
+seek = input("Enter the ending time: ")
 x = os.popen(f"yt-dlp --youtube-skip-dash-manifest -g {link}").read()
 x,y = x.split()
 cmd = "-ss 30" if rs(timestamp) != "00:00" or (rs(timestamp) == "00:00" and passed == True) else ""
-os.system(f"ffmpeg -ss {rs(timestamp)} -i \"{x}\" -ss {rs(timestamp)} -i \"{y}\" -map 0:v -map 1:a {cmd} -t {seek} -c:v libx264 -c:a aac output.mp4")
+os.system(f"ffmpeg -ss {rs(timestamp)} -i \"{x}\" -ss {rs(timestamp)} -i \"{y}\" -map 0:v -map 1:a {cmd} -t {diff(seek,timestamp)} -c:v libx264 -c:a aac output.mp4")
